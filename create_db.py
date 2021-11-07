80f816ef49b8ca567f1f4f5e317f80531375a7ab
Phil Hather <phil.hather@sso.purepla.net>
2021-11-07 19:16:50 +0000

    minor updates to csv_to_sqlite func

--- create_db.py
+++ create_db.py
@@ -9,14 +9,14 @@ def csv_to_sqlite(csv_fp, connection, tbl_name):
     except UnicodeDecodeError:  # CSV contains encoded special characters (poss written from Excel)
         print(f'Warning - encoding error discovered in {csv_fp}, removing bad characters and proceeding')
         df = pd.read_csv(csv_fp, index_col='Index', encoding_errors='ignore')
-        # Could also use "encoding='ISO-8859–1'" arg but preferred removal of special characters ready for db
+        # Could also use "encoding='cp1252'" arg but preferred removal of special characters ready for db
     print(f'Dataframe contains {len(df)} rows and {len(df.columns)} columns - headers are:\n{list(df.columns)}')
     df.to_sql(index_label='Index', name=tbl_name, con=connection, if_exists='replace')
     # Set to replace for dev purposes
     print('Dataframe successfully loaded to SQLite database')
 
 
-def execute_query(connection, sql_fp, ddl=False):
+def query_db(connection, sql_fp, ddl=False):
     """Executes a query against a SQLite database, returns the results as a dataframe, unless DDL"""
     with open(sql_fp, 'r') as file:
         query = file.read()
@@ -35,5 +35,5 @@ def execute_query(connection, sql_fp, ddl=False):
 if __name__ == '__main__':
     conn = sqlite3.connect('store.db')
     csv_to_sqlite('./SampleData.csv', conn, 'product')
-    execute_query(conn, './creator.sql', ddl=True)
+    query_db(conn, './creator.sql', ddl=True)
     conn.close()
